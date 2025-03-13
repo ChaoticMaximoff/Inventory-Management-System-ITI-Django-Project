b95d6a53bbfb88abc6ce9c9c011f4afcae9d84c9
belalnagy <belalnajy9@gmail.com>
2025-03-13 03:42:39 +0200

    feat: add navbar, update models, and create base template

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('', include('accounts.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
