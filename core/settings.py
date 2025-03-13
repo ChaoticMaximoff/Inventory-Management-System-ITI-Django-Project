9fefc6c08b2fa285eeb2dc038ab43bd0c7e714f8
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-13 21:11:06 +0200

    fix(auth): update LOGIN_URL to redirect to the correct login page
    Updated LOGIN_URL in settings.py to ensure unauthenticated users are redirected
    to the correct login page when trying to access protected views.

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+LOGIN_URL = "/"
 
 AUTHENTICATION_BACKENDS = [
     "accounts.backends.EmailBackend",
