9adbaf83d5322388ecd2342bb04b700279af8fa8
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-15 22:34:38 +0200

    refactor: move supermarket model to supermarket app

--- orders/models.py
+++ orders/models.py
@@ -1,32 +1,26 @@
 from django.db import models
 from inventory.models import Product
+from supermarkets.models import Supermarket
 
-
-class Supermarket(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    location = models.CharField(max_length=255, blank=True, null=True)
-    contact_number = models.CharField(max_length=20, blank=True, null=True)
-
-    def str(self):
-        return self.name
-    
 
 class Order(models.Model):
-    PENDING = 'PENDING'
-    CONFIRMED = 'CONFIRMED'
+    PENDING = "PENDING"
+    CONFIRMED = "CONFIRMED"
 
     STATUS_CHOICES = [
-        (PENDING, 'Pending'),
-        (CONFIRMED, 'Confirmed'),
+        (PENDING, "Pending"),
+        (CONFIRMED, "Confirmed"),
     ]
 
-    supermarket = models.ForeignKey(Supermarket, on_delete=models.RESTRICT, related_name="orders")
+    supermarket = models.ForeignKey(
+        Supermarket, on_delete=models.RESTRICT, related_name="orders"
+    )
     created_at = models.DateField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
 
     def str(self):
         return f"Order to {self.supermarket.name} - {self.status}"
-    
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
@@ -34,7 +28,9 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
 
     def save(self, *args, **kwargs):
-        if self.order.status == Order.CONFIRMED:  # Prevent modifications to confirmed orders
+        if (
+            self.order.status == Order.CONFIRMED
+        ):  # Prevent modifications to confirmed orders
             raise ValueError("Cannot modify a confirmed order")
         if self.quantity > self.product.quantity:
             raise ValueError("Not enough stock available")
--- supermarkets/models.py
+++ supermarkets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Supermarket(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+
+    def str(self):
+        return self.name
