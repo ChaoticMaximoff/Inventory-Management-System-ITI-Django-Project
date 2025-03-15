672533b74c9a23d06abcbfc0d564935032824c4c
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-15 22:59:33 +0200

    refactor: supermarket model str method

--- supermarkets/models.py
+++ supermarkets/models.py
@@ -5,5 +5,5 @@ class Supermarket(models.Model):
     name = models.CharField(max_length=255, unique=True)
     location = models.CharField(max_length=255, blank=True, null=True)
 
-    def str(self):
+    def __str__(self):
         return self.name
