a803854de1f01489190de9222b0420a4e3a40f8c
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-15 22:59:11 +0200

    add: URL patterns for supermarket views

--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("shipments/", include("shipments.urls")),
     path("factories/", include("factories.urls")),
     path("inventory/", include("inventory.urls", namespace="inventory")),
+    path("supermarkets/", include("supermarkets.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(
