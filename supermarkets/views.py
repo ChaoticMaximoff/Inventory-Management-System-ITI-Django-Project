78482adfa0d41ad908ac435aa9c79a61cd0060f4
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-15 22:41:31 +0200

    feat: supermarket view functions

--- supermarkets/views.py
+++ supermarkets/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+)
+from django.urls import reverse_lazy
+from supermarkets.models import Supermarket
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class SupermarketListView(LoginRequiredMixin, ListView):
+    model = Supermarket
+    template_name = "supermarkets/supermarket_list.html"
+    context_object_name = "supermarkets"
+    paginate_by = 5
+
+
+class SupermarketDetailView(LoginRequiredMixin, DetailView):
+    model = Supermarket
+    template_name = "supermarkets/supermarket_detail.html"
+    context_object_name = "supermarket"
+
+
+class SupermarketCreateView(LoginRequiredMixin, CreateView):
+    model = Supermarket
+    fields = ["name", "location"]
+    template_name = "supermarkets/supermarket_form.html"
+    success_url = reverse_lazy("supermarket_list")
+
+
+class SupermarketUpdateView(LoginRequiredMixin, UpdateView):
+    model = Supermarket
+    fields = ["name", "location"]
+    template_name = "supermarkets/supermarket_form.html"
+    success_url = reverse_lazy("supermarket_list")
+
+
+class SupermarketDeleteView(LoginRequiredMixin, DeleteView):
+    model = Supermarket
+    template_name = "supermarkets/supermarket_confirm_delete.html"
+    success_url = reverse_lazy("supermarket_list")
