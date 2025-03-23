0e7ebfe6193ac3f4eeef403ffd83b8835bda8539
belalnagy <belalnajy9@gmail.com>
2025-03-23 12:13:04 +0200

    Add permissions to register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import LoginView, RegisterView, index, dashboard
+from .views import LoginView, RegisterView, index, DashboardView,UserLogoutView
 app_name = 'accounts'
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('home/', index, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
 
