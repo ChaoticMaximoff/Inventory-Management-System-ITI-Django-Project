96c9c08e23fd4d2bf838c2fd53d88520a66b8b0c
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-24 06:22:57 +0200

    feat: Add filtering functionality to orders app

--- orders/views.py
+++ orders/views.py
@@ -3,53 +3,70 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 import sweetify
 from .models import Order, OrderItem, Supermarket
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin #for permissions
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+)  # for permissions
 from django.views import View
 from django.contrib import messages
 from .forms import OrdersForm, OrderItemForm
 from django.core.paginator import Paginator
+from .filters import OrderFilter
 
 
-#custom mixin for the required rules
+# custom mixin for the required rules
 class RoleRequiredMixin(UserPassesTestMixin):
     required_roles = []
-    def test_func(self): #checking if the user has the required roles
+
+    def test_func(self):  # checking if the user has the required roles
         user = self.request.user
         if not user.is_authenticated:
             return False
         if user.is_superuser:
             return True
-        if 'EMPLOYEE' in self.required_roles and user.role.upper() == 'MANAGER':
+        if "EMPLOYEE" in self.required_roles and user.role.upper() == "MANAGER":
             return True
+
     def handle_no_permission(self):
-        messages.error(self.request, "You do not have the permission to access this page.")
+        messages.error(
+            self.request, "You do not have the permission to access this page."
+        )
         return redirect("orders")
 
-class OrderListView(ListView):
+
+class OrderListView(LoginRequiredMixin, ListView):
     model = Order
-    template_name= 'orders/order_list.html'
-    context_object_name ='orders'
-    #ordering = ["-created_at"]
-    #paginate_by = 2
+    template_name = "orders/order_list.html"
+    context_object_name = "orders"
+    paginate_by = 8
 
     def get_queryset(self):
-        return Order.objects.order_by('-status') #---> for ordering
+        queryset = Order.objects.order_by("status")
+        self.filterset = OrderFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
 
- 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filter"] = self.filterset
+        return context
+
+
 class OrderDetailsView(DetailView):
     model = Order
-    template_name = 'orders/order_details.html'
-    context_object_name = 'order_items'
+    template_name = "orders/order_details.html"
+    context_object_name = "order_items"
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
-    
+
 
 class OrdersCreateView(LoginRequiredMixin, View):
-    #required_roles=["EMPLOYEE"] #any employee can create an order
+    # required_roles=["EMPLOYEE"] #any employee can create an order
     def get(self, request):
         form = OrdersForm()
-        return render(request, "orders/order_form.html", {"form":form, "mode":"create"})
+        return render(
+            request, "orders/order_form.html", {"form": form, "mode": "create"}
+        )
 
     def post(self, request):
         form = OrdersForm(request.POST)
@@ -58,14 +75,19 @@ class OrdersCreateView(LoginRequiredMixin, View):
             order.created_by_user = request.user
             order.save()
             return redirect("orders")
-        return render(request, "orders/order_form.html", {"form":form, "mode":"create"})
+        return render(
+            request, "orders/order_form.html", {"form": form, "mode": "create"}
+        )
+
 
 class OrderCreateItemView(LoginRequiredMixin, View):
     def get(self, request, pk):
         order = get_object_or_404(Order, id=pk, status="PENDING")
         form = OrderItemForm()
-        return render(request, "orders/order_item_form.html", {"form":form, "order":order})
-    
+        return render(
+            request, "orders/order_item_form.html", {"form": form, "order": order}
+        )
+
     def post(self, request, pk):
         order = get_object_or_404(Order, id=pk, status="PENDING")
         form = OrderItemForm(request.POST)
@@ -75,15 +97,17 @@ class OrderCreateItemView(LoginRequiredMixin, View):
             item.order = order
             item.save()
             return redirect("orders")
-        return render(request, "order/order_item_form.html", {"form":form, "order":order})
-    
+        return render(
+            request, "order/order_item_form.html", {"form": form, "order": order}
+        )
+
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
-    
-            
+
+
 class OrderConfirmView(LoginRequiredMixin, View):
     def post(self, request, pk):
-        order = get_object_or_404(Order, id=pk, status='PENDING')
+        order = get_object_or_404(Order, id=pk, status="PENDING")
 
         if not order.items.exists():
             sweetify.error(
@@ -104,7 +128,7 @@ class OrderConfirmView(LoginRequiredMixin, View):
             Order.created_at = Order.created_at
             product.save()
 
-        order.status = 'CONFIRMED'
+        order.status = "CONFIRMED"
         order.save()
         messages.success(request, "order confirmed! Stock updated.")
         return redirect("order_items", pk=order.id)
@@ -112,30 +136,34 @@ class OrderConfirmView(LoginRequiredMixin, View):
 
 class SupermarketOrderListView(ListView):
     model = Order
-    template_name = 'order/order_detail.html'
-    context_object_name = 'orders'
+    template_name = "order/order_detail.html"
+    context_object_name = "orders"
 
     def get_queryset(self):
-        supermarket_id = self.kwargs['supermarket_id']
-        return Order.objects.filter(supermarket_id=supermarket_id).order_by('-created_at')
+        supermarket_id = self.kwargs["supermarket_id"]
+        return Order.objects.filter(supermarket_id=supermarket_id).order_by(
+            "-created_at"
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['supermarket'] = Supermarket.objects.get(id=self.kwargs['supermarket_id'])
+        context["supermarket"] = Supermarket.objects.get(
+            id=self.kwargs["supermarket_id"]
+        )
         return context
-    
+
 
 class OrderUpdateView(LoginRequiredMixin, UpdateView):
     model = Order
     form_class = OrdersForm
-    template_name = 'orders/order_form.html'
-    success_url = reverse_lazy('orders')
-    
+    template_name = "orders/order_form.html"
+    success_url = reverse_lazy("orders")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["mode"] = "update"
         return context
-    
+
     def dispatch(self, request, *args, **kwargs):
         self.object = self.get_object()
         if self.object.status == "CONFIRMED":
@@ -150,9 +178,8 @@ class OrderUpdateView(LoginRequiredMixin, UpdateView):
                 showConfirmButton=False,
             )
             return redirect("orders")
-        
-        return super().dispatch(request, *args, **kwargs)
 
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         sweetify.success(
@@ -165,11 +192,11 @@ class OrderUpdateView(LoginRequiredMixin, UpdateView):
             showConfirmButton=False,
         )
         return super().form_valid(form)
-    
+
 
 class OrderDeleteView(LoginRequiredMixin, DeleteView):
     model = Order
-    success_url = reverse_lazy('orders')
+    success_url = reverse_lazy("orders")
 
     def post(self, request, *args, **kwargs):
         try:
@@ -186,7 +213,7 @@ class OrderDeleteView(LoginRequiredMixin, DeleteView):
                     showConfirmButton=False,
                 )
                 return redirect("orders")
-            
+
             self.object.delete()
             sweetify.success(
                 request,
@@ -199,7 +226,7 @@ class OrderDeleteView(LoginRequiredMixin, DeleteView):
                 showConfirmButton=False,
             )
             return redirect("orders")
-        
+
         except Exception as e:
             sweetify.error(
                 request,
@@ -212,14 +239,14 @@ class OrderDeleteView(LoginRequiredMixin, DeleteView):
                 showConfirmButton=False,
             )
             return redirect("orders")
-        
+
 
 class OrderItemDeleteView(LoginRequiredMixin, DeleteView):
     model = OrderItem
 
     def get_success_url(self):
-        return reverse_lazy('order_items', kwargs={"pk": self.object.order.pk})
-    
+        return reverse_lazy("order_items", kwargs={"pk": self.object.order.pk})
+
     def post(self, request, *args, **kwargs):
         orderitem = self.get_object()
         order = orderitem.order
@@ -237,7 +264,7 @@ class OrderItemDeleteView(LoginRequiredMixin, DeleteView):
             )
 
             return redirect("order_items", pk=order.pk)
-        
+
         orderitem.delete()
         sweetify.success(
             request,
