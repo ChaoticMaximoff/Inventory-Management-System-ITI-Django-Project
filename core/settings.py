921a0b55d0ef5532fbdbaaf6b55289553d843750
Hagar Galal <hagargalalstu@gmail.com>
2025-03-25 08:13:18 +0200

    update setting.py

--- core/settings.py
+++ core/settings.py
@@ -139,8 +139,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-MEDIA_URL = '/media'
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
